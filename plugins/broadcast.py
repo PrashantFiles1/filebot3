2f7b59c3bf3df20158e001f7c8b3b285304243c0
Prashant Yadav <109992225+PrashantFiles1@users.noreply.github.com>
2023-02-02 02:40:22 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -7,7 +7,7 @@ from info import ADMINS
 from utils import broadcast_messages
 import asyncio
         
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+@Client.on_messagefilters.command("broadcast") & filters.private & filters.chat(ADMINS))
 # https://t.me/LazyDeveoper
 async def verupikkals(bot, message):
     users = await db.get_all_users()
